04cf79676bd8ae65f710ed59fad84389fbd8c6c2
Evgen Kazankov <evgen.kazza@gmail.com>
2021-07-09 16:41:18 +0400

    Парсит предметы в командную строку

---
+++ src/main/java/twentytwentyone/ParserConsole/App.java
@@ -0,0 +1,23 @@
+package twentytwentyone.ParserConsole;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class App {
+    public static void main( String[] args ) throws MalformedURLException, IOException {
+    	Parser parser = new Parser();
+    	String url = "https://www.samgups.ru/raspisanie/2020-2021/vtoroy-semestr/HTML/73.html";
+    	
+    	Document page = parser.getPage(url);
+    	
+    	Elements tables = parser.getTables(page);
+    	Elements tableCols = parser.getColumns(tables);
+    	Elements tableRows = parser.getRows(tableCols);    
+    	Elements subjects = parser.getSubjects(tableRows);
+    	
+    	parser.readSubjects(subjects);
+    }
+}
